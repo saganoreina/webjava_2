5164ee431239dc7d8bc39512d26c4e069a0f7c3d
saganoreina <saganor@systena.co.jp>
2019-11-23 19:20:59 +0900

    再コミット

--- src/main/java/jp/co/systena/tigerscave/tatakau/controller/TatakauController.java
+++ src/main/java/jp/co/systena/tigerscave/tatakau/controller/TatakauController.java
@@ -1,25 +1,54 @@
 package jp.co.systena.tigerscave.tatakau.controller;
 
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public  class TatakauController {
+public class TatakauController {
 
-
+  @Autowired
+  HttpSession session; // セッション管理
 
 
   @RequestMapping(value = "/makenai", method = RequestMethod.GET) // URLとのマッピング
-  public ModelAndView makenai(ModelAndView mav) {
+  public ModelAndView getMakenai(ModelAndView mav) {
 
-//     Map<Integer, Item> syokugyouList = listService.getItemList();
-//
-//     mav.addObject("syokugyouList", itemList);
-//
-       mav.setViewName("tatakau");
+    // Map<Integer, Item> syokugyouList = listService.getItemList();
+    //
+    // mav.addObject("syokugyouList", itemList);
+    //
+    mav.setViewName("tatakau");
 
-     return mav;
+    return mav;
   }
- }
+
+  @RequestMapping(value = "/makenai", method = RequestMethod.POST) // URLとのマッピング
+  public ModelAndView postMakenai(ModelAndView mav) {
+    
+    
+    // ここでリクエストの内容を確認する。
+    
+    
+    // 問題なければセッションにリクエストパラメーターを入れる。
+    
+    
+    // 値を返す。
+    // テンプレートを呼び出す。
+    
+    // 
+    
+    
+    
+    // Map<Integer, Item> syokugyouList = listService.getItemList();
+    //
+    // mav.addObject("syokugyouList", itemList);
+    //
+    mav.setViewName("tatakau");
+
+    return mav;
+  }
+}

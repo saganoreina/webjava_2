e70a3f54dbd5553371be45439f07979743c4cb28
local_3p4pons <local_3p4pons@LAPTOP-NJLCBKL2>
2019-11-30 17:11:30 +0900

    コントローラーにメソッドを追加

--- src/main/java/jp/co/systena/tigerscave/tatakau/controller/TatakauController.java
+++ src/main/java/jp/co/systena/tigerscave/tatakau/controller/TatakauController.java
@@ -1,7 +1,5 @@
 package jp.co.systena.tigerscave.tatakau.controller;
 
-import javax.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,45 +8,69 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TatakauController {
 
-  @Autowired
-  HttpSession session; // セッション管理
+	// キャラ作成画面を送る
+	@RequestMapping(value = "/create/chara", method = RequestMethod.GET)
+	public ModelAndView getMakenai(ModelAndView mav) {
 
 
-  @RequestMapping(value = "/makenai", method = RequestMethod.GET) // URLとのマッピング
-  public ModelAndView getMakenai(ModelAndView mav) {
 
-    // Map<Integer, Item> syokugyouList = listService.getItemList();
-    //
-    // mav.addObject("syokugyouList", itemList);
-    //
-    mav.setViewName("tatakau");
+		mav.setViewName("tatakau");
+		return mav;
+	}
 
-    return mav;
-  }
+	@RequestMapping(value = "/create/", method = RequestMethod.GET)
 
-  @RequestMapping(value = "/makenai", method = RequestMethod.POST) // URLとのマッピング
-  public ModelAndView postMakenai(ModelAndView mav) {
-    
-    
-    // ここでリクエストの内容を確認する。
-    
-    
-    // 問題なければセッションにリクエストパラメーターを入れる。
-    
-    
-    // 値を返す。
-    // テンプレートを呼び出す。
-    
-    // 
-    
-    
-    
-    // Map<Integer, Item> syokugyouList = listService.getItemList();
-    //
-    // mav.addObject("syokugyouList", itemList);
-    //
-    mav.setViewName("tatakau");
+	// 送られてきたキャラクター情報と敵の情報保存してから、キャラクター一覧画面を送る
 
-    return mav;
-  }
+
+
+	// キャラクターこうげき画面を送る
+
+	// キャラクターかいふく画面を送る
+
+
+
+
+
+//  @Autowired
+//  HttpSession session; // セッション管理
+//
+//
+//  @RequestMapping(value = "/makenai", method = RequestMethod.GET) // URLとのマッピング
+//  public ModelAndView getMakenai(ModelAndView mav) {
+//
+//    // Map<Integer, Item> syokugyouList = listService.getItemList();
+//    //
+//    // mav.addObject("syokugyouList", itemList);
+//    //
+//    mav.setViewName("tatakau");
+//
+//    return mav;
+//  }
+//
+//  @RequestMapping(value = "/makenai", method = RequestMethod.POST) // URLとのマッピング
+//  public ModelAndView postMakenai(ModelAndView mav) {
+//
+//
+//    // ここでリクエストの内容を確認する。
+//
+//
+//    // 問題なければセッションにリクエストパラメーターを入れる。
+//
+//
+//    // 値を返す。
+//    // テンプレートを呼び出す。
+//
+//    //
+//
+//
+//
+//    // Map<Integer, Item> syokugyouList = listService.getItemList();
+//    //
+//    // mav.addObject("syokugyouList", itemList);
+//    //
+//    mav.setViewName("tatakau");
+//
+//    return mav;
+//  }
 }
